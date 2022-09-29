edf3ff549854f3c20f482b160178a60f6fdc4c1c
Kiran2821996 <kirandchennai@gmail.com>
2022-09-29 11:14:07 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React,{ useState } from "react";
 
 function App() {
+  const alphabet1 = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
+  const alphabet2 = ["a", "s", "d", "f", "g", "h", "j", "k", "l"]
+  const alphabet3 = ["z", "x", "c", "v", "b", "n", "m"]
+
+  const [capslock,setCapslock] =useState(false)
+  const [text, setText] = useState("")
+  console.log(text)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <textarea value={text}> 
+          
+        </textarea>
+      </div>
+      <div>
+        {alphabet1.map((item, index) => {
+          return (
+            <button value={ capslock?item.toUpperCase():item.toLowerCase()} onClick={(e) => setText(text + e.target.value)}>{capslock?item.toUpperCase():item.toLowerCase()}</button>
+          )
+        })}
+      </div>
+      <div>
+        {alphabet2.map((item, index) => {
+          return (
+            <button value={ capslock?item.toUpperCase():item.toLowerCase()}onClick={(e) => setText(text + e.target.value)}>{capslock?item.toUpperCase():item.toLowerCase()}</button>
+          )
+        })}
+      </div>
+      <div>
+        {alphabet3.map((item, index) => {
+          return (
+            <button value={ capslock?item.toUpperCase():item.toLowerCase()} onClick={(e) => setText(text + e.target.value)}>{capslock?item.toUpperCase():item.toLowerCase()}</button>
+          )
+        })}
+      </div>
+      <div>
+        <button onClick={()=>{
+          setText(text.slice(0,-1))
+        }}>BackSpace</button>
+        <button value=" " onClick={(e) => setText(text + e.target.value)}>Space</button>
+        <button onClick={()=>{
+          capslock?setCapslock(false):setCapslock(true)
+        }}>capslock</button>
+        
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
